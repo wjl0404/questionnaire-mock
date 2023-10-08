a27ccd557c124f2019eec8dd6c055572900b9675
wei jiale <812516872@qq.com>
2023-10-09 01:27:35 +0800

    config

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ async function getResponse(fn, ctx) {
   });
 }
 router.get("/", (ctx) => {
+  ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
   ctx.body = "hello";
 });
 mockList.forEach((item) => {
   const { url, method, response } = item;
   router[method](url, async (ctx) => {
     const res = await getResponse(response, ctx);
+    ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+    ctx.set('Access-Control-Allow-Credentials', 'true');
     ctx.body = res;
   });
 });
